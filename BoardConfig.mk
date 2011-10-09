bc1a481a118175958891cc966b5c512050ef0ef4
Pavel Chernyak <fjfalcon@fjfalcon.ru>
2011-10-09 19:36:30 +0400

    initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,108 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+SHOLEST_BUILD_USB_APK = false
+SHOLEST_BUILD_OMX = false
+SHOLEST_KOREAN_IME = false
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+USE_CAMERA_STUB := false
+
+# inherit from the proprietary version
+-include vendor/motorola/sholest/BoardConfigVendor.mk
+
+BOARD_USES_GENERIC_AUDIO := false
+TARGET_NO_BOOTLOADER := true
+#TARGET_NO_KERNEL := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_BOARD_PLATFORM := omap3
+TARGET_OTA_NO_KERNEL := true
+TARGET_OTA_EXTRA_ARGS := -e device/motorola/sholest/install-orbootstrap --no_kernel true --no_recovery true --backup false --override_device sholest
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8
+TARGET_OMAP3 := true
+COMMON_GLOBAL_CFLAGS += -DTARGET_OMAP3
+TARGET_BOOTLOADER_BOARD_NAME := sholest
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER := CUSTOM
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := libCustomWifi
+WPA_SUPPLICANT_VERSION      := VER_0_6_X
+BOARD_WLAN_DEVICE           := tiwlan0
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/tiwlan_drv.ko"
+BOARD_WLAN_TI_STA_DK_ROOT   := system/wlan/ti/wilink_6_1
+BOARD_USES_ECLAIR_LIBAUDIO := true
+#BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00380000
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
+#BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0afa0000   # limited so we enforce room to grow
+#BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0c520000
+#BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BOOTIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00380000)
+BOARD_RECOVERYIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00500000)
+BOARD_SYSTEMIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x0afa0000)
+BOARD_USERDATAIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x0cac0000)
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_USE_FROYO_LIBCAMERA := true
+HARDWARE_OMX := true
+BUILD_WITH_TI_AUDIO := 1
+BUILD_PV_VIDEO_ENCODERS := 1
+BOARD_HAVE_BLUETOOTH := true
+BOARD_EGL_CFG := device/motorola/sholest/egl.cfg
+# Changes related to bootmenu
+# Recovery
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_RECOVERY_IGNORE_BOOTABLES := true
+BOARD_HAS_SMALL_RECOVERY := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+#TARGET_RECOVERY_UI_LIB := librecovery_ui_sholes librecovery_ui_generic
+
+BOARD_NEVER_UMOUNT_SYSTEM := true
+#TARGET_RECOVERY_UI_LIB := librecovery_ui_generic
+#TARGET_RECOVERY_UPDATER_LIBS += librecovery_updater_generic
+
+USE_SHOLES_PROPERTY := true
+#TARGET_NO_KERNEL := true
+# Jordan need 2nd-init binary from motorola common
+TARGET_NEEDS_MOTOROLA_HIJACK := true
+#TARGET NO KERNEL from Quarx advice:
+
+# Prebuilt boot.img
+LOCAL_KERNEL := device/motorola/sholest/kernel
+PRODUCT_COPY_FILES += \
+	$(LOCAL_KERNEL):kernel
+
+ #In nighly builds only
+# Override cyanogen squisher to customize our update zip package
+ TARGET_CUSTOM_RELEASETOOL := ./device/motorola/sholest/releasetools/squisher
+#
+ifndef CYANOGEN_RELEASE
+
+
+
+endif
+
---
+++ releasetools/squisher.sh
@@ -0,0 +1,55 @@
+# This script is included in squisher
+# It is the final build step (after OTA package)
+
+# Delete unwanted apps
+rm -f $REPACK/ota/system/app/CMStats.apk
+rm -f $REPACK/ota/system/app/CMUpdateNotify.apk
+rm -f $REPACK/ota/system/app/RomManager.apk
+rm -f $REPACK/ota/system/xbin/irssi
+    
+# Location Proxy injection
+printf "Injecting smali..."
+(
+SMALID=$OUT/smali.d
+rm -rf $SMALID
+mkdir -p $SMALID
+cd $SMALID
+unzip $QUIET $REPACK/ota/system/framework/framework.jar "classes.dex"
+java -Xmx512m \
+	-jar $ANDROID_BUILD_TOP/vendor/cyanogen/tools/baksmali.jar classes.dex -o framework
+cp -fr $ANDROID_BUILD_TOP/device/motorola/sholest/smali/framework/* framework
+java -Xmx512m \
+	-jar $ANDROID_BUILD_TOP/vendor/cyanogen/tools/smali.jar framework -o classes.dex
+zip $QUIET $REPACK/ota/system/framework/framework.jar "classes.dex"
+rm -f classes.dex
+unzip $QUIET $REPACK/ota/system/framework/services.jar "classes.dex"
+java -Xmx512m \
+	-jar $ANDROID_BUILD_TOP/vendor/cyanogen/tools/baksmali.jar classes.dex -o services
+cp -fr $ANDROID_BUILD_TOP/device/motorola/sholest/smali/services/* services
+sed -i '/Location Proxy Service/,/I/ c\
+    const-string v9, "Starting Location Proxy."\
+\
+    invoke-static {v5, v9}, Landroid/util/Slog;->i(Ljava/lang/String;Ljava/lang/String;)I\
+\
+    const-string v5, "locationproxy"\
+\
+    new-instance v9, Lcom/android/server/LocationProxyService;\
+\
+    invoke-direct {v9, v6}, Lcom/android/server/LocationProxyService;-><init>(Landroid/content/Context;)V\
+\
+    invoke-static {v5, v9}, Landroid/os/ServiceManager;->addService(Ljava/lang/String;Landroid/os/IBinder;)V' services/com/android/server/ServerThread.smali
+java -Xmx512m \
+	-jar $ANDROID_BUILD_TOP/vendor/cyanogen/tools/smali.jar services -o classes.dex
+zip $QUIET $REPACK/ota/system/framework/services.jar "classes.dex"
+rm -rf $SMALID
+)
+echo
+
+# prebuilt boot, devtree, logo & updater-script
+#cp -f $ANDROID_BUILD_TOP/vendor/motorola/sholest/proprietary/boot.img $REPACK/ota/
+cp -f $ANDROID_BUILD_TOP/device/motorola/sholest/updater-script $REPACK/ota/META-INF/com/google/android/updater-script
+
+# Save build manifest into update
+repo manifest -r -o $REPACK/ota/system/repo-default.xml
+gzip $REPACK/ota/system/repo-default.xml
+
