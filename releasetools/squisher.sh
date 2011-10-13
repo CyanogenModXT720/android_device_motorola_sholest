15730acd4addcc1905332932f3b69e19aa855c43
Pavel Chernyak <fjfalcon@fjfalcon.ru>
2011-10-13 09:33:01 +0400

    Updater-script changes

--- releasetools/squisher.sh
+++ releasetools/squisher.sh
@@ -50,6 +50,6 @@ echo
 cp -f $ANDROID_BUILD_TOP/device/motorola/sholest/updater-script $REPACK/ota/META-INF/com/google/android/updater-script
 
 # Save build manifest into update
-repo manifest -r -o $REPACK/ota/system/repo-default.xml
-gzip $REPACK/ota/system/repo-default.xml
+#repo manifest -r -o $REPACK/ota/system/repo-default.xml
+#gzip $REPACK/ota/system/repo-default.xml
 
