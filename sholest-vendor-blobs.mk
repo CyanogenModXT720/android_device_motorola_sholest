5450f42c5c7852c19969fb3a2afcd0054ff1c76e
peshovec <jlesev@gmail.com>
2012-01-17 10:30:12 +0200

    follow nadlabak, gps lib from defy gb fw

--- sholest-vendor-blobs.mk
+++ sholest-vendor-blobs.mk
@@ -14,11 +14,11 @@
 
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES := \
-    device/motorola/sholest/proprietary/lib/libgps.so:obj/lib/libgps.so \
     device/motorola/sholest/proprietary/lib/libcamera.so:obj/lib/libcamera.so \
     device/motorola/sholest/proprietary/lib/libaudio.so:obj/lib/libaudio.so \
     device/motorola/sholest/proprietary/lib/libril-moto-umts-1.so:obj/lib/libril-moto-umts-1.so \
-    device/motorola/sholest/proprietary/lib/hw/gralloc.omap3.so:obj/lib/hw/gralloc.omap3.so
+    device/motorola/sholest/proprietary/lib/hw/gralloc.omap3.so:obj/lib/hw/gralloc.omap3.so \
+    device/motorola/sholest/proprietary/lib/hw/gps.sholes.so:obj/lib/hw/gps.sholes.so \
 
 # All the blobs necessary 
 # copy all files under the "proprietary" directory to system
