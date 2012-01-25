4f4186a67f07c5b41d473096ddcb299edd4769c4
Mioze7Ae <Mioze7Ae@gmail.com>
2012-01-24 23:04:00 -0500

    Set updater-script banner text automatically from modversion
    Change-Id: I58554303a9ba9bf7828d59e7c6c2af66a8a12a27

--- releasetools/squisher.sh
+++ releasetools/squisher.sh
@@ -8,7 +8,9 @@ rm -f $REPACK/ota/system/xbin/irssi
 
 # prebuilt boot, devtree, logo & updater-script
 #cp -f $ANDROID_BUILD_TOP/vendor/motorola/sholest/proprietary/boot.img $REPACK/ota/
-cp -f $ANDROID_BUILD_TOP/device/motorola/sholest/updater-script $REPACK/ota/META-INF/com/google/android/updater-script
+
+MODVERSION=`sed -n -e'/ro\.modversion/s/^.*CyanogenMod-/CyanogenMod-/p' $REPACK/ota/system/build.prop`
+sed -e "s/MODVERSION/$MODVERSION/g" $ANDROID_BUILD_TOP/device/motorola/sholest/updater-script > $REPACK/ota/META-INF/com/google/android/updater-script
 
 # Save build manifest into update
 
