2f52423a3f58fccc537be6725ef0b3d41001f7ea
Pavel Chernyak <fjfalcon@fjfalcon.ru>
2012-02-25 23:57:30 +0400

    Fixed usb first time plug bug
    Change-Id: Idd886ab70ae71c8148353bc84e13de3b45c6d30a

--- prebuilt/bin/sh_hijack.sh
+++ prebuilt/bin/sh_hijack.sh
@@ -2,6 +2,8 @@
 
 # Uncomment to enable logcat during boot
 #   /system/xbin/busybox echo "msc_adb" > /dev/usb_device_mode
+   /system/xbin/busybox touch /tmp/usbd-state
+   /system/xbin/busybox echo "usb_mode_msc_adb" > /tmp/usbd-state
    /system/xbin/mount -o remount,rw rootfs /
    /system/xbin/rmdir /config
    /system/xbin/rmdir /sdcard
