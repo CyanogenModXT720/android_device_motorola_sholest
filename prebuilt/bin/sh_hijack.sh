4a40041aba68136e6b4cae86a2a0103a555314bc
Pavel Chernyak <fjfalcon@chernyak.marathon.global>
2012-03-12 17:31:20 +0400

    2nd-init, sh_hijack.sh, link mksh -> sh

---
+++ prebuilt/bin/sh_hijack.sh
@@ -0,0 +1,18 @@
+#!/system/bin/sh
+
+#turn on logcat during boot
+   /system/xbin/busybox echo "msc_adb" > /dev/usb_device_mode
+   /system/xbin/busybox touch /tmp/usbd-state
+   /system/xbin/busybox echo "usb_mode_msc_adb" > /tmp/usbd-state
+   /system/xbin/mount -o remount,rw rootfs /
+   /system/xbin/rmdir /config
+   /system/xbin/rmdir /sdcard
+   /system/xbin/cp -fr /etc/rootfs/* /
+
+#   echo "-16" > /proc/$$/oom_adj
+
+   /system/xbin/umount /dev/cpuctl
+   /system/xbin/umount /sqlite_stmt_journals
+   /system/xbin/umount /dev/pts
+
+   exec /system/bin/2nd-init
