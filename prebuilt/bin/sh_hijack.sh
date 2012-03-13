e387bdd0d76c65f2756b2e1c1fa2c6654640078e
peshovec <jlesev@gmail.com>
2012-03-13 13:14:47 +0200

    use different from msc_adb default usb mode i suspect that msc_adb is suspicious hope will fix the need of second plug (just the first time)

--- prebuilt/bin/sh_hijack.sh
+++ prebuilt/bin/sh_hijack.sh
@@ -3,7 +3,7 @@
    /system/xbin/busybox echo "msc_adb" > /dev/usb_device_mode
    /system/xbin/busybox touch /tmp/usbd-state
    /system/xbin/busybox chown system.system /tmp/usbd-state
-   /system/xbin/busybox echo "usb_mode_msc_adb" > /tmp/usbd-state
+   /system/xbin/busybox echo "usb_mode_ngp_adb" > /tmp/usbd-state
    /system/xbin/mount -o remount,rw rootfs /
    /system/xbin/rmdir /config
    /system/xbin/rmdir /sdcard
