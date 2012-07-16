e714d9dd1758d0838e16005468ad4fa298086e09
Pavel fjfalcon Chernyak <fjfalcon@fjfalcon.ru>
2012-07-16 16:30:50 +0400

    Cleanup, fix sdcard

--- vibrator.c
+++ vibrator.c
@@ -22,7 +22,7 @@
 #include <errno.h>
 #define THE_DEVICE "/sys/class/timed_output/vibrator/enable"
 #define THE_LINEAR_DEVICE "/sys/class/timed_output/lvibrator/enable"
-#define MAX_LINEAR_DEVICE 150
+#define MAX_LINEAR_DEVICE 1500
 
 static int vibe(char *dev, int timeout_ms)
 {
